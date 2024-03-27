a54fbd66f1011ba15334cae67222b68f39b684b6
seongbeom <bum090877@gmail.com>
2024-03-27 17:39:59 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import fbDB from './connectors/fbDB';
 const app: Express = express();
 const port = process.env.PORT;
 
-
-
 app.get('/', (req: Request, res: Response) => {
   res.send('Typescript + Node.js + Express Server');
 });
